8cd92d314e069a46e97c14303f95560f004f4d9a
sohag95 <roysohag95@gmail.com>
2020-11-04 01:12:18 +0530

    eight  commit

--- controllers/passwordController.js
+++ controllers/passwordController.js
@@ -1,59 +1,60 @@
-const Password = require("../models/password")
+const Password = require("../models/Password")
 
-exports.changePassword=function(req,res){
-    let profileType=req.regNumber.slice(5,7)
-    if(req.body.passwordNew1 == req.body.passwordNew2){
-      let password=new Password(req.body)
-      password.changePassword(req.regNumber).then(()=>{
+exports.changePassword = function (req, res) {
+  let profileType = req.regNumber.slice(5, 7)
+  if (req.body.passwordNew1 == req.body.passwordNew2) {
+    let password = new Password(req.body)
+    password
+      .changePassword(req.regNumber)
+      .then(() => {
         req.flash("success", "You have successfully updated your new password.")
-        req.session.save(function() {
-            if(profileType=="st"){
-                res.redirect("/student-home")
-            }
-            if(profileType=="ad"){
-                res.redirect("/admin-home")
-            }
-            if(profileType=="te"){
-                res.redirect("/teacher-home")
-            }
-            if(profileType=="mc"){
-                res.redirect("/matchController-home")
-            }
-        })
-      }).catch((e)=>{
-        req.flash("errors", e)
-        req.session.save(function() {
-            if(profileType=="st"){
-                res.redirect(`/student/profile/${req.regNumber}/edit`)
-            }
-            if(profileType=="ad"){
-                res.redirect("/admin-home")
-            }
-            if(profileType=="te"){
-                res.redirect(`/teacher/profile/${req.regNumber}/edit`)
-            }
-            if(profileType=="mc"){
-                res.redirect("/matchController-home")
-            }
-          
-        })
-      })
-    }else{
-      req.flash("errors", "Your new passwords are not matching.Enter again.....")
-      req.session.save(function() {
-        if(profileType=="st"){
-            res.redirect(`/student/profile/${req.regNumber}/edit`)
-        }
-        if(profileType=="ad"){
+        req.session.save(function () {
+          if (profileType == "st") {
+            res.redirect("/student-home")
+          }
+          if (profileType == "ad") {
             res.redirect("/admin-home")
-        }
-        if(profileType=="te"){
-            res.redirect(`/teacher/profile/${req.regNumber}/edit`)
-        }
-        if(profileType=="mc"){
+          }
+          if (profileType == "te") {
+            res.redirect("/teacher-home")
+          }
+          if (profileType == "mc") {
             res.redirect("/matchController-home")
-        }
+          }
+        })
       })
-    }
-    
+      .catch(e => {
+        req.flash("errors", e)
+        req.session.save(function () {
+          if (profileType == "st") {
+            res.redirect(`/student/profile/${req.regNumber}/edit`)
+          }
+          if (profileType == "ad") {
+            res.redirect("/admin-home")
+          }
+          if (profileType == "te") {
+            res.redirect(`/teacher/profile/${req.regNumber}/edit`)
+          }
+          if (profileType == "mc") {
+            res.redirect("/matchController-home")
+          }
+        })
+      })
+  } else {
+    req.flash("errors", "Your new passwords are not matching.Enter again.....")
+    req.session.save(function () {
+      if (profileType == "st") {
+        res.redirect(`/student/profile/${req.regNumber}/edit`)
+      }
+      if (profileType == "ad") {
+        res.redirect("/admin-home")
+      }
+      if (profileType == "te") {
+        res.redirect(`/teacher/profile/${req.regNumber}/edit`)
+      }
+      if (profileType == "mc") {
+        res.redirect("/matchController-home")
+      }
+    })
   }
+}
